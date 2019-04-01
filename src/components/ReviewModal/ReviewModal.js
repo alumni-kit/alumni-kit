eb65d18171e2072381aff1c74ddc0ae92838c845
arellaTV <jeremiah.arella@gmail.com>
2019-03-31 23:11:00 -0700

    Renders possible persons

--- src/components/ReviewModal/ReviewModal.js
+++ src/components/ReviewModal/ReviewModal.js
@@ -1,4 +1,4 @@
-import { Button, Modal } from "semantic-ui-react";
+import { Button, Card, Image, List, Modal } from "semantic-ui-react";
 import React, { Component } from 'react';
 
 class ReviewModal extends Component {
@@ -30,10 +30,50 @@ class ReviewModal extends Component {
                     <Modal.Header>Review</Modal.Header>
                     <Modal.Content className="review-modal__content">
                         <p>This is the review Modal</p>
-                        <p>Selected row: {this.props.App.state.selectedRow.id}</p>
+                        <p>Selected row id: {this.props.App.state.selectedRow.id}</p>
+                        <Card.Group>
+                        {this.props.App.state.selectedRow.Status &&
+                            this.props.App.state.selectedRow.Status.response &&
+                                this.props.App.state.selectedRow.Status.response.possible_persons &&
+                                this.props.App.state.selectedRow.Status.response.possible_persons.map((person) => {
+                                    return (
+                                        <Card fluid key={person['@search_pointer']}>
+                                            <Card.Content>
+                                                {person.images && person.images[0] &&
+                                                    (<Image floated='right' size='mini' src={person.images[0].url} onError={(e) => console.log("Error!", e.target.style = "display: none")}/>)
+                                                }
+                                                {person.names && person.names[0] &&
+                                                    (<Card.Header>{person.names[0].display}</Card.Header>)
+                                                }
+                                                {person['@match'] &&
+                                                    (<Card.Meta>Match confidence level: {person['@match']}</Card.Meta>)
+                                                }
+                                            </Card.Content>
+                                            {(person.dob || person.educations || person.jobs) &&
+                                                <Card.Content>
+                                                    <List bulleted>
+                                                        {person.dob &&
+                                                            (<List.Item>{person.dob.display}</List.Item>)
+                                                        }
+                                                        {person.educations && person.educations[0] &&
+                                                            (<List.Item>{person.educations[0].display}</List.Item>)
+                                                        }
+                                                        {person.jobs && person.jobs[0] &&
+                                                            (<List.Item>{person.jobs[0].display}</List.Item>)
+                                                        }
+                                                    </List>
+                                                </Card.Content>
+                                            }
+                                            <Card.Content extra>
+                                                <Button onClick={this.openConfirmModal}>Start Follow-up Pipl Search</Button>
+                                            </Card.Content>
+                                        </Card>
+                                    )
+                                })
+                        }
+                        </Card.Group>
                     </Modal.Content>
                     <Modal.Actions>
-                        <Button onClick={this.openConfirmModal}>Start Pipl Search</Button>
                         <Button color="yellow" onClick={this.close}>Cancel</Button>
                     </Modal.Actions>
                 </Modal>
