126e7a619076c34ab12ac3108d976cbb4491bdef
arellaTV <jeremiah.arella@gmail.com>
2019-04-16 07:56:51 -0700

    Fixes bugs with completion modal and early exit modal

--- src/components/CompletionModal/CompletionModal.js
+++ src/components/CompletionModal/CompletionModal.js
@@ -1,23 +1,50 @@
 import React, { Component } from "react";
-import { Button, Modal } from "semantic-ui-react";
+import { Button, Dimmer, Loader, Modal } from "semantic-ui-react";
 
 class CompletionModal extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            dimmerActive: true,
+            completedSearches: null,
+            partialSearches: null,
+            errorSearches: null,
             open: false
         }
     }
 
     componentWillReceiveProps(props) {
+        const { App } = props;
+
         this.setState({ open: props.openCompletionModal });
+
+        setTimeout(() => {
+            let completedSearches = 0;
+            let partialSearches = 0;
+            let errorSearches = 0;
+            App.state.rows.forEach(row => {
+                if (typeof row.Status === "object" && row.Status.status && row["Last Update"]) {
+                    if (row.Status.status === "Complete") {
+                        completedSearches++;
+                    }
+
+                    if (row.Status.status === "Partial") {
+                        partialSearches++;
+                    }
+
+                    if (row.Status.status === "Error") {
+                        errorSearches++;
+                    }
+                }
+            });
+            this.setState({ completedSearches, partialSearches, errorSearches, dimmerActive: false });
+        }, 500);
     }
 
     close = () => this.props.App.setState({ openCompletionModal: false });
 
     render() {
-        const { App } = this.props;
         return (
             <Modal
                 id="progress-modal"
@@ -26,7 +53,12 @@ class CompletionModal extends Component {
             >
             <Modal.Header>SEARCH COMPLETE</Modal.Header>
             <Modal.Content className="progress-modal__content">
-                <p>{App.state.rows.length} successful matches</p>
+                <Dimmer active={this.state.dimmerActive}>
+                    <Loader />
+                </Dimmer>
+                <p>{this.state.completedSearches} complete matches</p>
+                <p>{this.state.partialSearches} partial matches</p>
+                <p>{this.state.errorSearches} errors</p>
             </Modal.Content>
             <Modal.Actions>
                 <Button color="yellow" onClick={this.close}>Continue</Button>
--- src/components/EarlyExitModal/EarlyExitModal.js
+++ src/components/EarlyExitModal/EarlyExitModal.js
@@ -1,17 +1,45 @@
 import React, { Component } from "react";
-import { Button, Modal } from "semantic-ui-react";
+import { Button, Dimmer, Loader, Modal } from "semantic-ui-react";
 
 class EarlyExitModal extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            dimmerActive: true,
+            completedSearches: null,
+            partialSearches: null,
+            errorSearches: null,
             open: false
         }
     }
 
     componentWillReceiveProps(props) {
-        this.setState({ open: props.openEarlyExitModal });
+        const { App } = props;
+
+        this.setState({ open: props.openEarlyExitModal, dimmerActive: true });
+
+        setTimeout(() => {
+            let completedSearches = 0;
+            let partialSearches = 0;
+            let errorSearches = 0;
+            App.state.totalRows.forEach(row => {
+                if (typeof row.Status === "object" && row.Status.status && row["Last Update"]) {
+                    if (row.Status.status === "Complete") {
+                        completedSearches++;
+                    }
+
+                    if (row.Status.status === "Partial") {
+                        partialSearches++;
+                    }
+
+                    if (row.Status.status === "Error") {
+                        errorSearches++;
+                    }
+                }
+            });
+            this.setState({ completedSearches, partialSearches, errorSearches, dimmerActive: false });
+        }, 500);
     }
 
     close = () => this.props.App.setState({ openEarlyExitModal: false });
@@ -26,8 +54,13 @@ class EarlyExitModal extends Component {
             >
             <Modal.Header>SEARCH EXITED</Modal.Header>
             <Modal.Content className="progress-modal__content">
-                <p>{App.state.completedSearches} completed searches</p>
-                <p>{App.state.totalSearches - App.state.completedSearches} rows left unsearched</p>
+                <Dimmer active={this.state.dimmerActive}>
+                    <Loader />
+                </Dimmer>
+                <p>{this.state.completedSearches} completed matches</p>
+                <p>{this.state.partialSearches} partial matches</p>
+                <p>{this.state.errorSearches} errors</p>
+                <p>{App.state.totalRows.length - this.state.completedSearches} rows left unsearched</p>
             </Modal.Content>
             <Modal.Actions>
                 <Button color="yellow" onClick={this.close}>Continue</Button>
