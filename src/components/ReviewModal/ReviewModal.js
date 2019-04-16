0254792c9c89c1120dbf13b1aa76e47336723689
arellaTV <jeremiah.arella@gmail.com>
2019-04-16 08:57:07 -0700

    Re-enables detail in review modal

--- src/components/ReviewModal/ReviewModal.js
+++ src/components/ReviewModal/ReviewModal.js
@@ -23,32 +23,38 @@ class ReviewModal extends Component {
                 <Header>Status: {this.props.App.state.selectedRow.Status && this.props.App.state.selectedRow.Status.status || "Not yet searched"}</Header>
                 <Divider />
                 <ul>
-                    <li><b>Fields:</b>
+                    <li>
                         {this.props.App.state.selectedRow.Status && this.props.App.state.selectedRow.Status.previousRow &&
-                            <ul>
-                                {Object.entries(this.props.App.state.selectedRow).map((entry) => {
-                                    const property = entry[0];
-                                    const value = entry[1];
-                                    const previousRow = this.props.App.state.selectedRow.Status.previousRow || {};
-                                    if (typeof value !== "object" && previousRow[property] !== value) {
-                                        return (
-                                            <li>{property}: {previousRow[property] ||  <mark>previously blank</mark>} <Icon name="arrow right" /> {value || <mark>currently blank</mark>}</li>
-                                        )
-                                    }
-                                })}
-                            </ul>
+                            <>
+                                <b>Updated Fields:</b>
+                                <ul>
+                                    {Object.entries(this.props.App.state.selectedRow).map((entry) => {
+                                        const property = entry[0];
+                                        const value = entry[1];
+                                        const previousRow = this.props.App.state.selectedRow.Status.previousRow || {};
+                                        if (typeof value !== "object" && previousRow[property] !== value) {
+                                            return (
+                                                <li>{property}: {previousRow[property] ||  <mark>previously blank</mark>} <Icon name="arrow right" /> {value || <mark>currently blank</mark>}</li>
+                                            )
+                                        }
+                                    })}
+                                </ul>
+                            </>
                         ||
-                            <ul>
-                                {Object.entries(this.props.App.state.selectedRow).map((entry) => {
-                                    const property = entry[0];
-                                    const value = entry[1];
-                                    if (typeof value !== "object") {
-                                        return (
-                                            <li>{property}: {value || <mark>currently blank</mark>}</li>
-                                        )
-                                    }
-                                })}
-                            </ul>
+                            <>
+                                <b>Fields:</b>
+                                <ul>
+                                    {Object.entries(this.props.App.state.selectedRow).map((entry) => {
+                                        const property = entry[0];
+                                        const value = entry[1];
+                                        if (typeof value !== "object") {
+                                            return (
+                                                <li>{property}: {value || <mark>currently blank</mark>}</li>
+                                            )
+                                        }
+                                    })}
+                                </ul>
+                            </>
                         }
                     </li>
                     {this.props.App.state.selectedRow.Status && this.props.App.state.selectedRow.Status.missingColumns &&
@@ -90,91 +96,65 @@ class ReviewModal extends Component {
         )
     }
 
-    renderCurrentMatch = () => {
-        let searchPointerPerson = null;
-        if (this.props.App.state.selectedRow.Status &&
-            this.props.App.state.selectedRow.Status.searchPointerResponse &&
-            this.props.App.state.selectedRow.Status.searchPointerResponse.person) {
-                searchPointerPerson = this.props.App.state.selectedRow.Status.searchPointerResponse.person;
-        }
-
-        if (searchPointerPerson) {
-            return (
-                <Card.Group>
-                    <Card fluid color="green">
-                        <Card.Content>
-                            <Card.Header>Current match:</Card.Header>
-                            {searchPointerPerson.images && searchPointerPerson.images[0] &&
-                                (<Image floated='right' size='mini' src={searchPointerPerson.images[0].url} onError={e => e.target.style = "display: none"}/>)
-                            }
-                            {searchPointerPerson.names && searchPointerPerson.names[0] &&
-                                (<Card.Header>{searchPointerPerson.names[0].display}</Card.Header>)
-                            }
-                        </Card.Content>
-                        {(searchPointerPerson.dob || searchPointerPerson.educations || searchPointerPerson.jobs) &&
-                            <Card.Content>
-                                <List bulleted>
-                                    {searchPointerPerson.dob &&
-                                        (<List.Item>{searchPointerPerson.dob.display}</List.Item>)
-                                    }
-                                    {searchPointerPerson.educations && searchPointerPerson.educations[0] &&
-                                        (<List.Item>{searchPointerPerson.educations[0].display}</List.Item>)
-                                    }
-                                    {searchPointerPerson.jobs && searchPointerPerson.jobs[0] &&
-                                        (<List.Item>{searchPointerPerson.jobs[0].display}</List.Item>)
-                                    }
-                                </List>
-                            </Card.Content>
-                        }
-                    </Card>
-                </Card.Group>
-            )
-        }
-    }
-
     renderPossibleMatches = () => {
         if (this.props.App.state.selectedRow.Status) {
             return (
-                <Card.Group>
-                    {this.props.App.state.selectedRow.Status.response &&
-                        this.props.App.state.selectedRow.Status.response.possible_persons &&
-                        this.props.App.state.selectedRow.Status.response.possible_persons.map((person) => {
-                            return (
-                                <Card fluid key={person['@search_pointer']}>
-                                    <Card.Content>
-                                        {person.images && person.images[0] &&
-                                            (<Image floated='right' size='mini' src={person.images[0].url} onError={e =>  e.target.style = "display: none"}/>)
-                                        }
-                                        {person.names && person.names[0] &&
-                                            (<Card.Header>{person.names[0].display}</Card.Header>)
-                                        }
-                                        {person['@match'] &&
-                                            (<Card.Meta>Match confidence level: {person['@match']}</Card.Meta>)
-                                        }
-                                    </Card.Content>
-                                    {(person.dob || person.educations || person.jobs) &&
+                <>
+                    <Header>All Possible Matches</Header>
+                    <Divider />
+                    <Card.Group>
+                        {this.props.App.state.selectedRow.Status.response &&
+                            this.props.App.state.selectedRow.Status.response.possible_persons &&
+                            this.props.App.state.selectedRow.Status.response.possible_persons.map((person) => {
+                                return (
+                                    <Card fluid key={person['@search_pointer']}>
                                         <Card.Content>
-                                            <List bulleted>
-                                                {person.dob &&
-                                                    (<List.Item>{person.dob.display}</List.Item>)
-                                                }
-                                                {person.educations && person.educations[0] &&
-                                                    (<List.Item>{person.educations[0].display}</List.Item>)
-                                                }
-                                                {person.jobs && person.jobs[0] &&
-                                                    (<List.Item>{person.jobs[0].display}</List.Item>)
-                                                }
-                                            </List>
+                                            {person.images && person.images[0] &&
+                                                (<Image floated='right' size='mini' src={person.images[0].url} onError={e =>  e.target.style = "display: none"}/>)
+                                            }
+                                            {person.names && person.names[0] &&
+                                                (<Card.Header>{person.names[0].display}</Card.Header>)
+                                            }
+                                            {person['@match'] &&
+                                                (<Card.Meta>Match confidence level: {person['@match']}</Card.Meta>)
+                                            }
                                         </Card.Content>
-                                    }
-                                    <Card.Content extra>
-                                        <Button onClick={this.openConfirmModal}>Replace</Button>
-                                    </Card.Content>
-                                </Card>
-                            )
-                        })
-                    }
-                </Card.Group>
+                                        {(person.gender || person.usernames || person.addresses || person.dob || person.educations || person.jobs) &&
+                                            <Card.Content>
+                                                <List bulleted>
+                                                    {person.names && person.names[0] &&
+                                                        (<List.Item>Names: {person.names.map(name => name.display).join(", ")}</List.Item>)
+                                                    }
+                                                    {person.gender && person.gender.content &&
+                                                        (<List.Item>Gender: {person.gender.content}</List.Item>)
+                                                    }
+                                                    {person.usernames && person.usernames[0] &&
+                                                        (<List.Item>Usernames: {person.usernames.map(username => username.content).join(", ")}</List.Item>)
+                                                    }
+                                                    {person.dob &&
+                                                        (<List.Item>Age: {person.dob.display}</List.Item>)
+                                                    }
+                                                    {person.addresses && person.addresses[0] &&
+                                                        (<List.Item>Addresses: {person.addresses[0].display}</List.Item>)
+                                                    }
+                                                    {person.educations && person.educations[0] &&
+                                                        (<List.Item>Educations: {person.educations[0].display}</List.Item>)
+                                                    }
+                                                    {person.jobs && person.jobs[0] &&
+                                                        (<List.Item>Jobs: {person.jobs[0].display}</List.Item>)
+                                                    }
+                                                </List>
+                                            </Card.Content>
+                                        }
+                                        <Card.Content extra>
+                                            <Button onClick={this.openConfirmModal}>Replace</Button>
+                                        </Card.Content>
+                                    </Card>
+                                )
+                            })
+                        }
+                    </Card.Group>
+                </>
             )
         }
     }
@@ -192,8 +172,7 @@ class ReviewModal extends Component {
                     <Modal.Header>Review</Modal.Header>
                     <Modal.Content className="review-modal__content">
                         {this.renderStatus()}
-                        {/* {this.renderCurrentMatch()} */}
-                        {/* {this.renderPossibleMatches()} */}
+                        {this.renderPossibleMatches()}
                     </Modal.Content>
                     <Modal.Actions>
                         <Button color="blue" onClick={this.openConfirmModal}>{this.props.App.state.selectedRow["Last Update"] ? "Retry" : "Search"}</Button>
